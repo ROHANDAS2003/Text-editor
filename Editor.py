6c58ff49c9a4e76f1c22ff46b766e2efb9dbc71c
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2022-12-25 17:47:37 +0530

    Update Editor.py
    Created a tool bar with the icon and function to do undo action.

--- Editor.py
+++ Editor.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 import sys
 
 
@@ -10,6 +11,18 @@ class TextEditor(QMainWindow):
         self.editor.setFontPointSize(20)
         self.showMaximized()
         self.setWindowTitle('Text Editor')
+        self.create_tool_bar()
+
+    def create_tool_bar(self):
+        tool_bar = QToolBar()
+
+        undo_action = QAction(QIcon('undo.png'),'undo', self)
+        undo_action.triggered.connect(self.editor.undo)
+        tool_bar.addAction(undo_action)
+
+        self.addToolBar(tool_bar)
+
+
 app = QApplication(sys.argv)
 window = TextEditor()
 window.show()
