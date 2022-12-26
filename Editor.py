4b62f1fcc9944992c95cb438ea1ef1e5ecfec254
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2022-12-27 00:30:59 +0530

    Added a icon for cut
    Added icon and function to perform cut operation

--- Editor.py
+++ Editor.py
@@ -24,6 +24,12 @@ class TextEditor(QMainWindow):
         redo_action.triggered.connect(self.editor.redo)
         tool_bar.addAction(redo_action)
 
+        tool_bar.addSeparator()
+        tool_bar.addSeparator()
+
+        cut_action = QAction(QIcon('cut.png'), 'cut', self)
+        cut_action.triggered.connect(self.editor.cut)
+        tool_bar.addAction(cut_action)
         self.addToolBar(tool_bar)
 
 
