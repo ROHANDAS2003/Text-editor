d59f8580c1a49702e509db44649c342188671bfc
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2024-09-12 23:08:44 +0530

    .

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,27 @@
+QIcon("bold.png"), 'Bold', self)
+        # bold_action.triggered.connect(self.bold_text)
+        # tool_bar.addAction(bold_action)
+
+        # underline_action = QAction(QIcon("underline.png"), 'Underline', self)
+        # underline_action.triggered.connect(self.underline_text)
+        # tool_bar.addAction(underline_action)
+
+        # italic_action = QAction(QIcon("italic.png"), 'Italic', self)
+        # italic_action.triggered.connect(self.italic_text)
+        # tool_bar.addAction(italic_action)
+
+        # tool_bar.addSeparator()
+
+        # right_alignment_action = QAction(QIcon("right-align.png"), 'Align Right', self)
+        # right_alignment_action.triggered.connect(lambda : self.editor.setAlignment(Qt.AlignRight))
+        # tool_bar.addAction(right_alignment_action)
+
+        # left_alignment_action = QAction(QIcon("left-align.png"), 'Align Left', self)
+        # left_alignment_action.triggered.connect(lambda : self.editor.setAlignment(Qt.AlignLeft))
+        # tool_bar.addAction(left_alignment_action)
+
+        # justification_action = QAction(QIcon("justification.png"), 'Center/Justify', self)
+        # justification_action.triggered.connect(lambda : self.editor.setAlignment(Qt.AlignCenter))
+        # tool_bar.addAction(justification_action)
+
+        # self.addtool_bar(tool_bar)
